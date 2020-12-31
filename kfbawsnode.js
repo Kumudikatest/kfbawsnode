a8dfcfab042818e7404c36633bddd61c9166a856
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-12-31 13:03:38 +0530

    change lambda timeout

---
+++ kfbawsnode.js
@@ -0,0 +1,16 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    try {
+        let data = await ddb.scan({
+            TableName: "KChineseAnimal"
+        }).promise();
+console.log(data);
+    } catch (err) {
+        console.log(err);
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
+};
